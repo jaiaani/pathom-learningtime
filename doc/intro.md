226cb339e5e5b4c6887e9a3d72ab3e9685770911
Jaiane <jaiane.guimaraes@nubank.com.br>
2022-09-12 15:39:59 -0300

    :tada: Pathom introduction

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to learning-time
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/learning_time/core.clj
@@ -0,0 +1,31 @@
+(ns learning-time.core
+  (:require [com.wsscode.pathom.core :as p]
+            [com.wsscode.pathom.connect :as pc]))
+
+(pc/defresolver answer [_ _]
+                {::pc/output [:answer-to-everything]}
+                {:answer-to-everything 42})
+
+(pc/defresolver answer-plus-one [_ {:keys [answer-to-everything]}]
+                {::pc/input  #{:answer-to-everything}
+                 ::pc/output [:answer-plus-one]}
+                {:answer-plus-one (inc answer-to-everything)})
+
+(def registry
+  [answer answer-plus-one])
+
+(def parser
+  (p/parser
+    {::p/env     {::p/reader               [p/map-reader
+                                            pc/reader2
+                                            pc/open-ident-reader
+                                            p/env-placeholder-reader]
+                  ::p/placeholder-prefixes #{">"}}
+     ::p/mutate  pc/mutate
+     ::p/plugins [(pc/connect-plugin {::pc/register registry})
+                  p/error-handler-plugin
+                  p/trace-plugin]}))
+
+(comment
+  ; to call the parser and get some data out of it, run:
+  (parser {} [:answer-plus-one]))
